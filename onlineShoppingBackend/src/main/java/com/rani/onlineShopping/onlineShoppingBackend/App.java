ce2d3b90734f83bb8e69ecb1b29100cc2a030f3d
Rani Rawat <rani.rawat@ericsson.com>
2018-08-01 19:46:47 +0530

    My Project Setup Completed

---
+++ onlineShoppingBackend/src/main/java/com/rani/onlineShopping/onlineShoppingBackend/App.java
@@ -0,0 +1,13 @@
+package com.rani.onlineShopping.onlineShoppingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
